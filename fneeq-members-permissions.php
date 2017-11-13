3b81173d4968765cbd053dafe93950eb400e1a75
Michael Dean <mike@koumbit.org>
2017-11-13 11:57:04 -0500

    initial commit

---
+++ fneeq-members-permissions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     FNEEQ Custom Members Permissions
+ * Plugin URI:      https://github.com/mykedean/fneeq-members-permissions
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          Michael Dean
+ * Author URI:      https://github.com/mykedean
+ * Text Domain:     fneeq-members-permissions
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Fneeq_Members_Permissions
+ */
